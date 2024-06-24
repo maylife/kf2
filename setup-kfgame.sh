852cc684a7b2113aebb24400e46a7e113a47df35
Daniel Brooksby <daniel@darksidetcg.store>
2024-06-24 19:52:46 +1000

    Add support for bDisableTeamCollision

--- setup-kfgame.sh
+++ setup-kfgame.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-SERVER_NAME="$SERVER_NAME"
-SHORT_NAME="$SHORT_NAME"
-WEBSITE_LINK="$WEBSITE_LINK"
-BANNER_LINK="$BANNER_LINK"
-SERVER_MOTD="$SERVER_MOTD"
-CLAN_MOTTO="$CLAN_MOTTO"
-ADMIN_PASSWORD="$ADMIN_PASSWORD"
-GAME_PASSWORD="$GAME_PASSWORD"
+ServerName="$ServerName"
+ShortName="$ShortName"
+WebsiteLink="$WebsiteLink"
+BannerLink="$BannerLink"
+ServerMOTD="$ServerMOTD"
+ClanMotto="$ClanMotto"
+AdminPassword="$AdminPassword"
+GamePassword="$GamePassword"
+bDisableTeamCollision="$bDisableTeamCollision"
 
 CONFIG_FILE=./KFGame/Config/LinuxServer-KFGame.ini
 
 sed -i -e "s~^ServerName=.*~ServerName=$SERVER_NAME~g" "$CONFIG_FILE"
 sed -i -e "s~^ShortName=.*~ShortName=$SHORT_NAME~g" "$CONFIG_FILE"
-sed -i -e "s~^WebsiteLink=.*~WebsiteLink=$WEBSITE_LINK~g" "$CONFIG_FILE"
-sed -i -e "s~^BannerLink=.*~BannerLink=$BANNER_LINK~g" "$CONFIG_FILE"
-sed -i -e "s~^ServerMOTD=.*~ServerMOTD=$SERVER_MOTD~g" "$CONFIG_FILE"
-sed -i -e "s~^ClanMotto=.*~ClanMotto=$CLAN_MOTTO~g" "$CONFIG_FILE"
+sed -i -e "s~^WebsiteLink=.*~WebsiteLink=$WebsiteLink~g" "$CONFIG_FILE"
+sed -i -e "s~^BannerLink=.*~BannerLink=$BannerLink~g" "$CONFIG_FILE"
+sed -i -e "s~^ServerMOTD=.*~ServerMOTD=$ServerMOTD~g" "$CONFIG_FILE"
+sed -i -e "s~^ClanMotto=.*~ClanMotto=$ClanMotto~g" "$CONFIG_FILE"
 
-sed -i -e "s~^AdminPassword=.*~AdminPassword=$ADMIN_PASSWORD~g" "$CONFIG_FILE"
-sed -i -e "s~^GamePassword=.*~$GAME_PASSWORD~g" "$CONFIG_FILE"
+sed -i -e "s~^AdminPassword=.*~AdminPassword=$AdminPassword~g" "$CONFIG_FILE"
+sed -i -e "s~^GamePassword=.*~GamePassword=$GamePassword~g" "$CONFIG_FILE"
+
+sed -i -e "s~^bDisableTeamCollision=.*~bDisableTeamCollision=$bDisableTeamCollision~g" "$CONFIG_FILE"
+
 
 
 # Launch new game or the last played map
